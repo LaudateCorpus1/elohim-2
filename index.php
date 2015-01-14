4f675dddc64eca26b6c12334f8bf660ee922e180
devolutionary <devolutionary@devolutionary.net>
2015-01-15 09:55:35 +1300

    First Commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'init.inc.php';
 <head>
     <script src="//code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
-    <script src="app/scripts/mandala.js"></script>
+    <script src="app/mandala.js"></script>
     <script src="app/scripts/user.js"></script>
 </head>
 <body>
