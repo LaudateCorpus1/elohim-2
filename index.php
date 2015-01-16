c550cd41bb45735216bafebf3077c0823b5471cd
devolutionary <devolutionary@devolutionary.net>
2015-01-16 14:37:43 +1300

    Init Dev

--- index.php
+++ index.php
@@ -6,12 +6,23 @@ include INCLUDES_PATH.'header.inc.php';
 
 mysql_connect('localhost', 'mdcore_user', 'G3RjyH4/&oQ}?Oc');
 mysql_select_db('md_v3');
-$result = mysql_query('select * from merit_flaw where meritFlawStatus="restricted"');
+$result = mysql_query('select meritFlawName from merit_flaw where meritFlawIsUniversal=true');
+//$result = mysql_query('select venueName, meritFlawName from venue natural join venue_has_merit_flaw natural join merit_flaw');
 echo '<pre>';
 while ($row = mysql_fetch_assoc($result)) {
-    $id = preg_replace('/[^\da-z-]/i', '', preg_replace('/[\W]/i', '-', strtolower($row['meritFlawName'])));
-    echo '"'.$id.'": "'.$id.'"'.",\n";
+    $mfid = preg_replace('/[^\da-z_]/i', '', preg_replace('/[\W]/i', '_', strtolower($row['meritFlawName'])));
+    //$vnid = preg_replace('/[^\da-z_]/i', '', preg_replace('/[\W]/i', '_', strtolower($row['venueName'])));
+    //$vmf[$vnid][] = $mfid;
+    echo '"'.$mfid.'": "'.$mfid.'",'."\n";
+
 }
+
+//foreach ($vmf as $k => $r) {
+    //echo '"'.$k.'": {';
+
+    //foreach($r as $i)
+    //echo '},'."\n";
+//
 
 
 
