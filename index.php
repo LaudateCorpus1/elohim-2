ff955efc4291dd2abcd9fc480773fe66bf864f3a
devolutionary <devolutionary@devolutionary.net>
2015-01-16 14:17:04 +1300

    Init Dev

--- index.php
+++ index.php
@@ -4,9 +4,14 @@ include INCLUDES_PATH.'header.inc.php';
 
 
 
-
-
-
+mysql_connect('localhost', 'mdcore_user', 'G3RjyH4/&oQ}?Oc');
+mysql_select_db('md_v3');
+$result = mysql_query('select * from merit_flaw where meritFlawStatus="restricted"');
+echo '<pre>';
+while ($row = mysql_fetch_assoc($result)) {
+    $id = preg_replace('/[^\da-z-]/i', '', preg_replace('/[\W]/i', '-', strtolower($row['meritFlawName'])));
+    echo '"'.$id.'": "'.$id.'"'.",\n";
+}
 
 
 
