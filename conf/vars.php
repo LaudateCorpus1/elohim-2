b293b770babba3539fa611b614d51e625b0dd5ef
devolutionary <devolutionary@devolutionary.net>
2015-05-25 14:05:07 +1200

    New changelist

--- conf/vars.php
+++ conf/vars.php
@@ -1,5 +1,11 @@
 <?php
+define ('ROOT_PATH', 'http://localhost');
+define ('FILE_PATH', '');
+define ('SESSION_PATH', FILE_PATH.'/sessions');
 define ('CLASS_PATH', $__DIR.'classes/');
 define ('THEME_PATH', $__DIR.'theme/');
-define ('INCLUDES_PATH', THEME_PATH.'includes/');
+define ('THEME_INCLUDES_PATH', THEME_PATH.'includes/');
 define ('STYLES_PATH', THEME_PATH.'styles/');
+define ('SCRIPTS_PATH', THEME_PATH.'scripts/');
+define ('APP_PATH', $__DIR.'app/');
+define ('MODULES_PATH', THEME_INCLUDES_PATH.'modules/');
